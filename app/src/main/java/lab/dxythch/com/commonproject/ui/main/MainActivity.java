5767d5e169415654a53b4949aff0d76be08332fc
晏万华 <yanwanhua@wesmartclothing.com>
2018-05-07 19:49:20 +0800

    2018-05-07第一次提交

--- app/src/main/java/lab/dxythch/com/commonproject/ui/main/MainActivity.java
+++ app/src/main/java/lab/dxythch/com/commonproject/ui/main/MainActivity.java
@@ -1,12 +1,12 @@
 package lab.dxythch.com.commonproject.ui.main;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.flyco.tablayout.CommonTabLayout;
 import com.flyco.tablayout.listener.CustomTabEntity;
 import com.flyco.tablayout.listener.OnTabSelectListener;
-import com.vondear.rxtools.view.UnScrollableViewPager;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -29,8 +29,8 @@ public class MainActivity extends BaseActivity {
 
     @ViewById
     CommonTabLayout mCommonTabLayout;
-    @ViewById
-    UnScrollableViewPager mUnScrollableViewPager;
+//    @ViewById
+//    UnScrollableViewPager mUnScrollableViewPager;
 
     private ArrayList<CustomTabEntity> mBottomTabItems = new ArrayList<>();
     private List<Fragment> mFragments = new ArrayList<>();
@@ -43,33 +43,35 @@ public class MainActivity extends BaseActivity {
 
         initBottomTab();
         initMyViewPager();
+        setDefaultFragment();
     }
 
     private void initMyViewPager() {
+        mFragments.clear();
         mFragments.add(SlimmingFragment.getInstance());
         mFragments.add(FindFragment.getInstance());
         mFragments.add(StoreFragment.getInstance());
         mFragments.add(MineFragment.getInstance());
 
-        mUnScrollableViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
-            @Override
-            public Fragment getItem(int position) {
-                return mFragments.get(position);
-            }
-
-            @Override
-            public int getCount() {
-                return mFragments.size();
-            }
-        });
+//        mUnScrollableViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+//            @Override
+//            public Fragment getItem(int position) {
+//                return mFragments.get(position);
+//            }
+//
+//            @Override
+//            public int getCount() {
+//                return mFragments.size();
+//            }
+//        });
     }
 
     private void initBottomTab() {
         String[] tab_text = getResources().getStringArray(R.array.tab_text);
         int[] imgs_unselect = {R.mipmap.slimming_unselect, R.mipmap.found_normal_icon1,
                 R.mipmap.market_normal_icon1, R.mipmap.my_normal_icon1};
-        int[] imgs_select = {R.mipmap.fit_click_icon1, R.mipmap.found_click_icon1,
-                R.mipmap.market_click_icon1, R.mipmap.my_click_icon1};
+        int[] imgs_select = {R.mipmap.fit_click_icon1, R.mipmap.found_click_icon,
+                R.mipmap.market_click_icon, R.mipmap.my_click_icon1};
 
         for (int i = 0; i < tab_text.length; i++) {
             mBottomTabItems.add(new BottomTabItem(imgs_select[i], imgs_unselect[i], tab_text[i]));
@@ -80,7 +82,8 @@ public class MainActivity extends BaseActivity {
         mCommonTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelect(int position) {
-                mUnScrollableViewPager.setCurrentItem(position);
+//                mUnScrollableViewPager.setCurrentItem(position);
+                switchFragment(mFragments.get(position));
             }
 
             @Override
@@ -88,7 +91,30 @@ public class MainActivity extends BaseActivity {
 
             }
         });
+    }
 
+    private FragmentManager fm;
+    private Fragment mFragmentNow;
+
+    private void setDefaultFragment() {
+        fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.add(R.id.mFrameLayout, mFragments.get(0));
+        transaction.commit();
+        mFragmentNow = mFragments.get(0);
+    }
+
+
+    private void switchFragment(Fragment to) {
+        if (mFragmentNow != to) {
+            FragmentTransaction transaction = fm.beginTransaction();
+            if (!to.isAdded()) {    // 先判断是否被add过
+                transaction.hide(mFragmentNow).add(R.id.mFrameLayout, to).commitAllowingStateLoss(); // 隐藏当前的fragment，add下一个到Activity中
+            } else {
+                transaction.hide(mFragmentNow).show(to).commitAllowingStateLoss(); // 隐藏当前的fragment，显示下一个
+            }
+            mFragmentNow = to;
+        }
     }
 
 
