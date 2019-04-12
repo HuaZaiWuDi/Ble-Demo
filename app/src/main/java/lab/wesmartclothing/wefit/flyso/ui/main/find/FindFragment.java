b28b5149298163b5914f35befd6848df8fdb6532
晏万华 <yanwanhua@wesmartclothing.com>
2019-04-12 10:43:19 +0800

    解决瘦身衣发现页面收藏失败BUG

--- app/src/main/java/lab/wesmartclothing/wefit/flyso/ui/main/find/FindFragment.java
+++ app/src/main/java/lab/wesmartclothing/wefit/flyso/ui/main/find/FindFragment.java
@@ -1,8 +1,19 @@
 package lab.wesmartclothing.wefit.flyso.ui.main.find;
 
 import android.graphics.Bitmap;
-import android.webkit.ValueCallback;
+import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.github.lzyzsd.jsbridge.BridgeHandler;
+import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.BridgeWebViewClient;
+import com.github.lzyzsd.jsbridge.CallBackFunction;
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.MiddlewareWebClientBase;
 import com.umeng.socialize.ShareAction;
 import com.umeng.socialize.UMShareAPI;
 import com.umeng.socialize.UMShareListener;
@@ -17,83 +28,112 @@ import com.vondear.rxtools.view.RxToast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import lab.wesmartclothing.wefit.flyso.base.BaseWebTFragment;
+import butterknife.BindView;
+import lab.wesmartclothing.wefit.flyso.R;
+import lab.wesmartclothing.wefit.flyso.base.BaseAcFragment;
 import lab.wesmartclothing.wefit.flyso.netutil.net.ServiceAPI;
 import lab.wesmartclothing.wefit.flyso.tools.SPKey;
+import lab.wesmartclothing.wefit.flyso.utils.AndroidInterface;
 
 /**
  * Created icon_hide_password jk on 2018/5/7.
  */
-public class FindFragment extends BaseWebTFragment {
+public class FindFragment extends BaseAcFragment {
 
+    @BindView(R.id.parent)
+    RelativeLayout mParent;
+
+
+    private BridgeWebView mBridgeWebView;
+    private AgentWeb mAgentWeb;
 
     public static FindFragment getInstance() {
         return new FindFragment();
     }
 
+    @Override
+    public int layoutId() {
+        return R.layout.fragment_find;
+    }
 
     @Override
-    public void initViews() {
-        url = ServiceAPI.FIND_Addr;
+    protected void initViews() {
         super.initViews();
+        mBridgeWebView = new BridgeWebView(mActivity);
+        initWebView();
+    }
 
-        webView.evaluateJavascript("javascript:getUserId(" + SPUtils.getString(SPKey.SP_UserId) + ")", new ValueCallback<String>() {
+    private void initWebView() {
+        mBridgeWebView = new BridgeWebView(mActivity);
+
+        mAgentWeb = AgentWeb.with(this)//
+                .setAgentWebParent(mParent, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))//
+                .useDefaultIndicator(-1, 2)//
+                .setWebViewClient(new BridgeWebViewClient(mBridgeWebView))
+                .setWebView(mBridgeWebView)
+                .setMainFrameErrorView(R.layout.layout_web_error, -1) //参数1是错误显示的布局，参数2点击刷新控件ID -1表示点击整个布局都刷新， AgentWeb 3.0.0 加入。
+                .useMiddlewareWebClient(getMiddleWareWebClient())
+                .setSecurityType(AgentWeb.SecurityType.STRICT_CHECK)
+                .createAgentWeb()//
+                .ready()//
+                .go(ServiceAPI.FIND_Addr);
+        RxLogUtils.e("加载的URL：" + ServiceAPI.FIND_Addr);
+
+        // AgentWeb 没有把WebView的功能全面覆盖 ，所以某些设置 AgentWeb 没有提供 ， 请从WebView方面入手设置。
+        mAgentWeb.getWebCreator().getWebView().setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        WebSettings webSettings = mAgentWeb.getWebCreator().getWebView().getSettings();
+//        设置默认加载的可视范围是大视野范围
+        webSettings.setLoadWithOverviewMode(true);
+//                自适应屏幕(导致活动页面显示出错了)
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webSettings.setUseWideViewPort(true);
+
+        if (mAgentWeb != null)
+            mAgentWeb.getJsInterfaceHolder().addJavaObject("android", new AndroidInterface(mAgentWeb, mActivity));
+
+        mBridgeWebView.registerHandler("shareEvent", new BridgeHandler() {
             @Override
-            public void onReceiveValue(String s) {
-                RxLogUtils.d("返回的结果：" + s);
-            }
-        });
+            public void handler(final String data, CallBackFunction function) {
+                RxLogUtils.i("传递参数：" + data);
 
-        webView.registerHandler("shareEvent", (data, function) -> {
-            RxLogUtils.i("传递参数：" + data);
+                if (!RxUtils.isFastClick(1000)) {
+                    try {
+                        JSONObject object = new JSONObject(data);
+                        String img = object.getString("img");
+                        final String title = object.getString("title");
+                        final String desc = object.getString("desc");
+                        final String url = object.getString("url");
 
-            if (!RxUtils.isFastClick(1000)) {
-                try {
-                    JSONObject object = new JSONObject(data);
-                    String img = object.getString("img");
-                    final String title = object.getString("title");
-                    final String desc = object.getString("desc");
-                    final String url = object.getString("url");
-
-//                        showShareDialog(img, title, desc, url);
-                    showSimpleBottomSheetGrid(img, title, desc, url);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                        showSimpleBottomSheetGrid(img, title, desc, url);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
     }
 
+    protected MiddlewareWebClientBase getMiddleWareWebClient() {
+        return new MiddlewareWebClientBase() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
 
-    @Override
-    protected void onVisible() {
-        super.onVisible();
+                RxLogUtils.i("加载网页地址：" + url);
+                mAgentWeb.getJsAccessEntrace().quickCallJs("getUserId", SPUtils.getString(SPKey.SP_UserId));
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        };
     }
-
-
-    @Override
-    public void onStop() {
-//        onRelease();
-        super.onStop();
-    }
-
-    @Override
-    protected void initRxBus() {
-        super.initRxBus();
-//        //只有在显示时才会网络请求
-//        RxBus.getInstance().register2(NetWorkType.class)
-//                .compose(RxComposeUtils.<NetWorkType>bindLife(lifecycleSubject))
-//                .subscribe(new RxSubscriber<NetWorkType>() {
-//                    @Override
-//                    protected void _onNext(NetWorkType netWorkType) {
-//                        RxLogUtils.d("网络状态：" + netWorkType);
-//                        if (netWorkType.isBoolean()) {
-//                            webView.reload();
-//                        }
-//                    }
-//                });
-    }
-
 
     private void showSimpleBottomSheetGrid(final String imgUrl, final String title, final String desc, final String url) {
 
@@ -145,8 +185,8 @@ public class FindFragment extends BaseWebTFragment {
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
+        mAgentWeb.destroy();
         UMShareAPI.get(mContext).release();
+        super.onDestroy();
     }
-
 }
